aa9630053fd52249d750ccef5b1abb58052976ee
sliu00 <liushuai121998@gmail.com>
2018-09-17 10:24:32 +0800

    ...

---
+++ pages/user/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div>/user</div>
+        <a href="/user/one">to one</a>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
---
+++ pages/user/one.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>/user/one</div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
